e18b0723d6165555ad3f0df090e76f4060eb01eb
Huu Nguyen <huu@prismskylabs.com>
2014-04-29 13:03:50 -0700

    Initial source files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='hydrapy',
+    version='0.0.1',
+    url='https://github.com/whoshuu/hydra',
+    author='Huu Nguyen',
+    author_email='whoshuu@gmail.com',
+    description='A script for managing python versions.',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Unix Shell',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Operating System :: Unix',
+    ],
+    scripts=['hydra', ],
+)
